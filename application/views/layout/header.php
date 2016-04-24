b6eac0a70f6c98c6d4ade96030b5da3ae6a601ad
unknown <colbert@Colbert.redes.com>
2016-04-23 23:26:25 -0500

    creando

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -341,19 +341,4 @@
                 </section>
 
                 <!-- Main content -->
-                <section class="content">
-                    <h1>Hello nuevo</h1>
-                </section><!-- /.content -->
-            </aside><!-- /.right-side -->
-        </div><!-- ./wrapper -->
-
-
-        <!-- jQuery 2.0.2 -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <!-- Bootstrap -->
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- AdminLTE App -->
-        <script src="js/AdminLTE/app.js" type="text/javascript"></script>
-
-    </body>
-</html>
+                
