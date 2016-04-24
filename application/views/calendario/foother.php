f1b15436617ce30751532bea36e0a994bd3410fc
Sergio Renato <sergio.12.educando@gmail.com>
2016-04-24 11:39:53 -0500

    Agrenado mas cosas al calendario

--- application/views/calendario/foother.php
+++ application/views/calendario/foother.php
@@ -16,7 +16,7 @@
         <script src="<?= base_url(); ?>js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
 
         <script type="text/javascript">
-            $(function() {
+            $(document).ready(function() {
                 var fecha = new Date();
                 var ano = fecha.getFullYear();
                 $("#anio").val(ano);
@@ -29,6 +29,33 @@
                     "bSort": true,
                     "bInfo": false,
                     "bAutoWidth": false,
+                    "oLanguage" :{
+                        'sProcessing':     'Cargando...',
+                        'sLengthMenu':     'Mostrar _MENU_ registros',
+                        'sZeroRecords':    'No se encontraron resultados',
+                        'sEmptyTable':     'Ningún dato disponible en esta tabla',
+                        'sInfo':           'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+                        'sInfoEmpty':      'Mostrando registros del 0 al 0 de un total de 0 registros',
+                        'sInfoFiltered':   '(filtrado de un total de _MAX_ registros)',
+                        'sInfoPostFix':    '',
+                        'sSearch':         'Buscar:',
+                        'sUrl':            '',
+                        'sInfoThousands':  '',
+                        'sLoadingRecords': 'Cargando...',
+                        'oPaginate': {
+                            'sFirst':    'Primero',
+                            'sLast':     'Último',
+                            'sNext':     'Siguiente',
+                            'sPrevious': 'Anterior'
+                        },
+                        'oAria': {
+                            'sSortAscending':  ': Activar para ordenar la columna de manera ascendente',
+                            'sSortDescending': ': Activar para ordenar la columna de manera descendente'
+                        }
+                    },
+                    'aaSorting': [[ 0, 'asc' ]],//ordenar
+                    'iDisplayLength': 5,
+                    'aLengthMenu': [[5, 10, 20], [5, 10, 20]]
 
                 });
                 $("#avanzar").click(function(){
