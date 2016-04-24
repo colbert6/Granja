efb4dde9383358bb24caec63517cc5ef866f9b29
willy <lopez_leo_94@hotmail.com>
2016-04-23 23:06:53 -0500

    leonardo

--- application/views/foto.php
+++ application/views/foto.php
@@ -354,18 +354,6 @@
                                         <input type="text" class="form-control" id="codigo" placeholder="Ingrese codigo">
                                     </div>
                                     <div class="form-group">
-                                        <label>Raza</label>
-                                        <select class="form-control">
-
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Tipo Registro</label>
-                                        <select class="form-control">
-
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
                                         <label for="nombre">Nombre</label>
                                         <input type="text" class="form-control" id="nombre" placeholder="Ingrese nombre">
                                     </div>
@@ -409,8 +397,35 @@
                             </form>
                         </div>
                     </div>
-                    
+                    <div class="col-md-6">
+                        <div class="box box-warning">
+                            <div class="box-header">
+                                <h3 class="box-title">Galeria</h3>
+                            </div>
+                            <form role="form">
+                                <div class="box-body">
+                                    <div class="form-group">
+                                        <label for="codigo">Codigo</label>
+                                        <input type="text" class="form-control" id="codigo" placeholder="Ingrese codigo">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Animal</label>
+                                        <select class="form-control">
 
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="imagen">Imagen</label>
+                                        <input type="file" id="imagen">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="estado">Estado</label>
+                                        <input type="text" class="form-control" id="estado" placeholder="Ingrese estado">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
