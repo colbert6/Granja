c30ced16ef8c9ab9bd39cf215fc21958ffcad54a
colbert6 <colbersiho_6696@hotmail.com>
2016-04-25 09:06:53 -0500

    jsdh

--- application/views/animales/form.php
+++ application/views/animales/form.php
@@ -2,10 +2,11 @@
 <div class="col-md-6">
     <div class="box box-primary">
         <div class="box-header">
-            <h3 class="box-title">Animales</h3>
+        
         </div>
-        <form role="form" action="<?= base_url()."index.php/".$action ?>" method="post">
-            <input name="guardar" id="guardar" type="hidden" value="1">
+        <form role="form" action="<?= base_url()."index.php/".$action ?>" method="post" class="innline">
+            <div class="box-body">
+                <input name="guardar" id="guardar" type="hidden" value="1">
                 <div class="form-group">
                     <label for="codigo">Codigo</label>
                     <input type="text" class="form-control" id="codigo" name="codigo" placeholder="Ingrese codigo">
@@ -41,33 +42,45 @@
                     <label for="proveedor">Proveedor</label>
                     <input type="text" class="form-control" id="proveedor" name="proveedor" placeholder="Ingrese proveedor">
                 </div>
-                <div class="form-group">
-                    <label for="npadre">Nombre padre</label>
-                    <input type="text" class="form-control" id="padre" name="padre" placeholder="Ingrese nombre del padre">
+            </div>
+            <style type="text/css">
+                .mover{position: absolute;top: -2px;left: 530px}
+            </style>
+            <div class="col-lg-12 mover">
+                <div class="box box-warning">
+                     <div class="box-header">
+                    </div>
+                    <div class="box-body">
+                            <div class="form-group">
+                                <label for="npadre">Nombre padre</label>
+                                <input type="text" class="form-control" id="padre" name="padre" placeholder="Ingrese nombre del padre">
+                            </div>
+                            <div class="form-group">
+                                <label for="nmadre">Nombre madre</label>
+                                <input type="text" class="form-control" id="madre" name="madre" placeholder="Ingrese nombre de la madre">
+                            </div>
+                            <div class="form-group">
+                                <label for="sexo">Sexo</label>
+                                <input type="text" class="form-control" id="sexo" name="sexo" placeholder="Ingrese nombre de la madre">
+                            </div>
+                            <div class="form-group">
+                                <label for="fechanac">Fecha de nacimiento</label>
+                                <input type="date" class="form-control" id="fechanac" name="fechanac">
+                            </div>
+                            <div class="form-group">
+                                <label for="fechareg">Fecha de registro</label>
+                                <input type="date" class="form-control" id="fechareg" name="fechareg">
+                            </div>
+                            <div class="form-group">
+                                <label for="descripcion">Descripcion</label>
+                                <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder="Ingrese descripcion">
+                            </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="nmadre">Nombre madre</label>
-                    <input type="text" class="form-control" id="madre" name="madre" placeholder="Ingrese nombre de la madre">
-                </div>
-                <div class="form-group">
-                    <label for="sexo">Sexo</label>
-                    <input type="text" class="form-control" id="sexo" name="sexo" placeholder="Ingrese nombre de la madre">
-                </div>
-                <div class="form-group">
-                    <label for="fechanac">Fecha de nacimiento</label>
-                    <input type="date" class="form-control" id="fechanac" name="fechanac">
-                </div>
-                <div class="form-group">
-                    <label for="fechareg">Fecha de registro</label>
-                    <input type="date" class="form-control" id="fechareg" name="fechareg">
-                </div>
-                <div class="form-group">
-                    <label for="descripcion">Descripcion</label>
-                    <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder="Ingrese descripcion">
-                </div>
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                </div>
+            </div>
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
             
         </form>
     </div>
